699ee355f3651b90b6d2eb672f325c7b4dba0bfc
Joshua Ferndes <fernandes.developer@gmail.com>
2022-12-29 11:34:58 +0530

    Ability to ping

--- routes/resources.js
+++ routes/resources.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const gyan = require("./resources/gyan_route")
-const posts = require("./resources/posts_route")
-const course = require("./resources/course_route")
-const events = require("./resources/events_route")
-const guruForum = require("./resources/guruForum_route")
-const group = require("./resources/groups_route")
-const gpost = require("./resources/gposts_route")
-const comment = require("./resources/comment_route")
-const badge = require("./resources/badge_route")
-const userWall = require("./resources/userWall_route")
-const prayer = require("./resources/prayers_route")
+const gyan = require("./resources/gyan_route");
+const posts = require("./resources/posts_route");
+const course = require("./resources/course_route");
+const events = require("./resources/events_route");
+const guruForum = require("./resources/guruForum_route");
+const group = require("./resources/groups_route");
+const gpost = require("./resources/gposts_route");
+const comment = require("./resources/comment_route");
+const badge = require("./resources/badge_route");
+const userWall = require("./resources/userWall_route");
+const prayer = require("./resources/prayers_route");
 
 router.use("/gyan", gyan);
 router.use("/posts", posts);
 router.use("/course", course);
 router.use("/events", events);
-router.use("/guru-forum", guruForum)
+router.use("/guru-forum", guruForum);
 router.use("/gposts", gpost);
 router.use("/group", group);
 router.use("/comments", comment);
-router.use("/badges", badge)
+router.use("/badges", badge);
 router.use("/wall", userWall);
 router.use("/prayers", prayer);
+
+router.get("/ping", async (req, res) => {
+  res.send();
+});
 
 module.exports = router;
