426c363393ced4e0f478d556fc77a790a5190888
Joshua Ferndes <fernandes.developer@gmail.com>
2023-03-02 16:36:45 +0530

    Optimization

--- routes/resources/groups_route.js
+++ routes/resources/groups_route.js
@@ -4,26 +4,26 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 // const guru = require("../../middleware/guru");
 const validate = require("../../middleware/validate");
-const { Group, validatePost } = require("../../models/resources/group_model");
+const { Group } = require("../../models/resources/group_model");
 
 
 router.get("/", async (req, res) => {
   const group = await Group.find();
-  if (!group) return res.status(404).send("Post does not exist...");
+  if (!group) return res.status(404).send("Groups does not exist...");
   res.send(group);
 });
 
 router.get("/user", [auth], async (req, res) => {
   console.log(req.user._id)
   const group = await Group.find({ user: req.user._id });
-  if (!group) return res.status(404).send("Posts do not exist...");
+  if (!group) return res.status(404).send("Groups do not exist...");
   res.send(group);
 });
 
 
 router.get("/:id", async (req, res) => {
   const group = await Group.findById(req.params.id);
-  if (!group) return res.status(404).send("Post does not exist...");
+  if (!group) return res.status(404).send("Group does not exist...");
   res.send(group);
 });
 
@@ -47,7 +47,7 @@ router.put("/addUser:id", [auth], async (req, res) => {
   console.log(req.body);
   const group = await Group.findByIdAndUpdate(req.params.id, { usersAssociated: req.body.usersAssociated, memberCount: req.body.memberCount },
     { new: true, useFindAndModify: false, strict: false });
-  if (!group) return res.status(404).send("Event does not exist...");
+  if (!group) return res.status(404).send("Group does not exist...");
   res.send(group);
 });
 
@@ -56,7 +56,7 @@ router.put("/removeGroupManager:id", [auth], async (req, res) => {
   console.log(req.body);
   const group = await Group.findByIdAndUpdate(req.params.id, { groupManagers: req.body.groupManager },
     { new: true, useFindAndModify: false, strict: false });
-  if (!group) return res.status(404).send("Event does not exist...");
+  if (!group) return res.status(404).send("Group does not exist...");
   res.send(group);
 });
 
@@ -70,14 +70,14 @@ router.put("/addManagers:id", [auth], async (req, res) => {
     description: req.body.description, groupManagers: req.body.groupManagers,
     creatorName: req.body.creatorName
   }, { new: true, useFindAndModify: false, strict: false });
-  if (!group) return res.status(404).send("Event does not exist...");
+  if (!group) return res.status(404).send("Group does not exist...");
   res.send(group);
 });
 
-router.delete("/:id/", [auth], async (req, res) => {
+router.delete("/:id", [auth], async (req, res) => {
   console.log(req.params.id);
   const event = await Group.findByIdAndDelete(req.params.id, { new: false, useFindAndModify: false, strict: false });
-  if (!event) return res.status(404).send("Event does not exist...");
+  if (!event) return res.status(404).send("Group does not exist...");
   res.send(event);
 });
 
