9a27f411317b327f19083aec25b872b1dd4bfd21
Joshua Ferndes <fernandes.developer@gmail.com>
2023-03-24 15:25:38 +0530

    updated GPOST delte

--- routes/resources/gposts_route.js
+++ routes/resources/gposts_route.js
@@ -6,7 +6,6 @@ const auth = require("../../middleware/auth");
 const validate = require("../../middleware/validate");
 const { GPost, validatePost } = require("../../models/resources/gposts_model");
 
-
 router.get("/", async (req, res) => {
   const group = await GPost.find();
   if (!group) return res.status(404).send("Post does not exist...");
@@ -20,7 +19,16 @@ router.get("/:groupId", [auth], async (req, res) => {
 });
 
 router.post("/", [auth], async (req, res) => {
-  let group = GPost(_.pick(req.body, ["title", "groupPostImage", "description", "groupId", "userId", "createdDate"]));
+  let group = GPost(
+    _.pick(req.body, [
+      "title",
+      "groupPostImage",
+      "description",
+      "groupId",
+      "userId",
+      "createdDate",
+    ])
+  );
   try {
     group = await group.save();
     res.send({ ..._.pick(group, ["_id", "title"]) });
@@ -30,8 +38,15 @@ router.post("/", [auth], async (req, res) => {
 });
 
 router.post("/update", [auth], async (req, res) => {
-  const group = await GPost.findByIdAndUpdate(req.body.id, { title: req.body.title, description: req.body.description, groupPostImage: req.body.groupPostImage },
-    { new: true, useFindAndModify: false, strict: false });
+  const group = await GPost.findByIdAndUpdate(
+    req.body.id,
+    {
+      title: req.body.title,
+      description: req.body.description,
+      groupPostImage: req.body.groupPostImage,
+    },
+    { new: true, useFindAndModify: false, strict: false }
+  );
   if (!group) return res.status(404).send("Event does not exist...");
   res.send(group);
 });
@@ -43,23 +58,33 @@ router.get("/fetch/:id", [auth], async (req, res) => {
 });
 
 router.post("/incrementview", [auth], async (req, res) => {
-  const group = await GPost.findByIdAndUpdate(req.body._id, { views: req.body.views },
-    { new: true, useFindAndModify: false, strict: false });
+  const group = await GPost.findByIdAndUpdate(
+    req.body._id,
+    { views: req.body.views },
+    { new: true, useFindAndModify: false, strict: false }
+  );
   if (!group) return res.status(404).send("Event does not exist...");
   res.send(group);
 });
 
 router.post("/updatelikes", [auth], async (req, res) => {
-  const group = await GPost.findByIdAndUpdate(req.body._id, { likes: req.body.likes },
-    { new: true, useFindAndModify: false, strict: false });
+  const group = await GPost.findByIdAndUpdate(
+    req.body._id,
+    { likes: req.body.likes },
+    { new: true, useFindAndModify: false, strict: false }
+  );
   if (!group) return res.status(404).send("Event does not exist...");
   res.send(group);
 });
 
-router.delete("/:id/", [auth], async (req, res) => {
-  const event = await GPost.findByIdAndDelete(req.params.id, { new: false, useFindAndModify: false, strict: false });
-  if (!event) return res.status(404).send("Event does not exist...");
-  res.send(event);
+router.delete("/:id", [auth], async (req, res) => {
+  try {
+    const gpost = await GPost.findByIdAndDelete(req.params.id);
+    if (!gpost) return res.status(404).send("GPost does not exist...");
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 router.put("/post/incrementComments/:id", [auth], async (req, res) => {
@@ -67,8 +92,12 @@ router.put("/post/incrementComments/:id", [auth], async (req, res) => {
   if (!post) return res.status(404).send("Forum Post does not exist...");
 
   try {
-    post = await GPost.findByIdAndUpdate(post._id, { $set: { comments: post.comments + 1 } }, { new: true })
-    res.send(post)
+    post = await GPost.findByIdAndUpdate(
+      post._id,
+      { $set: { comments: post.comments + 1 } },
+      { new: true }
+    );
+    res.send(post);
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -83,7 +112,6 @@ router.post("/getUsersGroups", [auth], async (req, res) => {
   } catch (error) {
     res.status(400).send(error.message);
   }
-
 });
 
 // router.get("/:id", async (req, res) => {
@@ -92,9 +120,6 @@ router.post("/getUsersGroups", [auth], async (req, res) => {
 //   res.send(group);
 // });
 
-
-
-
 // router.post("/:id/:eventStatus", [auth], async (req, res) => {
 //   console.log(req.params.id);
 //   const group = await Group.findByIdAndUpdate(req.params.id, { eventStatus: req.params.eventStatus },
@@ -102,9 +127,6 @@ router.post("/getUsersGroups", [auth], async (req, res) => {
 //   if (!group) return res.status(404).send("Event does not exist...");
 //   res.send(group);
 // });
-
-
-
 
 // router.put("/addManagers:id", [auth], async (req, res) => {
 //   console.log(req.params);
@@ -124,4 +146,3 @@ router.post("/getUsersGroups", [auth], async (req, res) => {
 // });
 
 module.exports = router;
-
