4513ba5f44c808f11127903d9899546ae5356a49
Joshua Ferndes <fernandes.developer@gmail.com>
2023-04-21 11:52:07 +0530

    added OTP Login Route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const Joi = require("joi");
+const auth = require("../middleware/auth");
+const { sendPhoneVerification } = require("../services/twillio");
 
 router.post("/", async (req, res) => {
   console.log("in login")
@@ -16,9 +18,51 @@ router.post("/", async (req, res) => {
   console.log(validPassword)
   if (!validPassword) return res.status(400).send("Invalid Username or password..");
 
-  const token = user.generateAuthToken();
+  const token = user.generateAuthToken()
   res.send(token);
 });
+
+router.post("/requestOTP", async (req,res) =>{
+  let phone = req.query.phone
+  phone = "+" + phone.trim()
+  if (!phone) return res.status(404).send("Enter an PhoneNumber...");
+  console.log(phone)
+  let user = await User.findOne({ phone });
+  if (!user) return res
+    .status(400)
+    .send("Phone Number not found/Registered. Please check Phone Number.");
+  console.log(user._id, "User ID")
+  try {
+    let verification = await sendPhoneVerification(phone);
+    res.send(verification);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+})
+
+router.post("/loginWithOTP", async (req,res) =>{
+  let phone = req.body.phone
+  if (!phone || !req.body.otp) return res.status(404).send("Provide Phone Number or code...");
+    try {
+      let verification = await verifyPhoneCode(phone, req.body.otp);
+      if(verification.status === "approved"){
+        phone = "+" + phone.trim()
+        let user = await User.findOne({ phone })
+        const token = user.generateAuthToken()
+        res.send(token);
+      }else{
+        res.status(400).send("Invalid OTP");
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+})
+
+router.post("/tokenRefresh",[auth], async (req,res) =>{
+  const user = await User.findById(req.user._id).select("-password");
+  const token = user.generateAuthToken();
+  res.send(token);
+})
 
 async function validate(req) {
   const schema = Joi.object({
