d50c445dfa4d49e738360f3b56801b3adabf5d50
Joshua Ferndes <fernandes.developer@gmail.com>
2023-04-21 12:16:59 +0530

    Fix 5

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ phone: req.body.phone });
   if (!user) return res.status(400).send("Invalid Username or password..");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
