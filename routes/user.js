87c9febc171ac07607bfaf4840f65fd9ae19c62a
Joshua Ferndes <fernandes.developer@gmail.com>
2023-05-02 17:43:20 +0530

    Made email noptional

--- routes/user.js
+++ routes/user.js
@@ -133,11 +133,12 @@ router.post("/", async (req, res) => {
   const { error } = UserValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ phone: req.body.phone });
+  
   if (user)
     return res
       .status(400)
-      .send("Email already registered. Please Use another email.");
+      .send("Phone already registered. Please Use another Phone no.");
 
   if (req.body.isGuru) {
     // user = User(_.pick(req.body, ["firstName", "lastName", "email", "password", "isGuru", "isSubAdmin", "isAdmin"]));
