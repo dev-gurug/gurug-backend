af5fd08506fd3bb23e9fca12b0ecdb9fb9dc89fd
Joshua Ferndes <fernandes.developer@gmail.com>
2023-05-22 13:41:51 +0530

    added CrossOrgin ristriction 1

--- startup/routes.js
+++ startup/routes.js
@@ -5,11 +5,11 @@ const admin = require("../routes/admin");
 const resources = require("../routes/resources");
 const user_actions = require("../routes/user_actions");
 const error = require("../middleware/error");
-const cors = require("cors");
+// const cors = require("cors");
 
 module.exports = function(app) {
   //----------------------Setting route handlers--------------------------
-  app.use(cors({ origin: "https://www.guru-g.app" }));
+  // app.use(cors({ origin: "https://www.guru-g.app" }));
   app.use(express.json());
   app.use("/api/users", users);
   app.use("/api/auth", auth);
