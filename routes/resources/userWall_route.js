0047b4b131c83048e7f87abf4111e1149c1c038a
Joshua Ferndes <fernandes.developer@gmail.com>
2023-08-18 17:55:47 +0530

    changed Data limit in user wall

--- routes/resources/userWall_route.js
+++ routes/resources/userWall_route.js
@@ -20,7 +20,7 @@ router.get("/myWall", [auth], async (req, res) => {
   if (language === englishId) english = true;
 
   var cutoff = new Date();
-  cutoff.setDate(cutoff.getDate() - 30);
+  cutoff.setDate(cutoff.getDate() - 10);
 
   const three_days_query = { createdDate: { $gt: cutoff } };
   let resourses = [];
